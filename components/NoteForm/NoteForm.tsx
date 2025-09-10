2ed7aa5657943bcf77780770277e8b8d23fb0ed7
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-09-10 16:51:52 +0300

    Done 1

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -3,10 +3,10 @@ import { ErrorMessage, Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import type { Tag } from "@/types/note";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useEffect } from "react";
 
 import { createNote } from "@/lib/api";
 import { useNoteDraftStore } from "@/lib/noteStore";
+import DraftSaver from "../DraftSaver/DraftSaver";
 
 export interface NoteValues {
   title: string;
@@ -61,75 +61,47 @@ export default function NoteForm({ onClose }: NoteFormProps) {
       onSubmit={handleSubmit}
       enableReinitialize
     >
-      {({ values }) => {
-        useEffect(() => {
-          setDraft(values);
-        }, [values, setDraft]);
+      <Form className={css.form}>
+        <DraftSaver setDraft={setDraft} />
+        <fieldset className={css.formGroup}>
+          <label htmlFor="title">Title</label>
+          <Field id="title" type="text" name="title" className={css.input} />
+          <ErrorMessage component="span" name="title" className={css.error} />
+        </fieldset>
 
-        return (
-          <Form className={css.form}>
-            <fieldset className={css.formGroup}>
-              <label htmlFor="title">Title</label>
-              <Field
-                id="title"
-                type="text"
-                name="title"
-                className={css.input}
-              />
-              <ErrorMessage
-                component="span"
-                name="title"
-                className={css.error}
-              />
-            </fieldset>
+        <fieldset className={css.formGroup}>
+          <label htmlFor="content">Content</label>
+          <Field
+            id="content"
+            name="content"
+            rows={8}
+            className={css.textarea}
+            as="textarea"
+          />
+          <ErrorMessage component="span" name="content" className={css.error} />
+        </fieldset>
 
-            <fieldset className={css.formGroup}>
-              <label htmlFor="content">Content</label>
-              <Field
-                id="content"
-                name="content"
-                rows={8}
-                className={css.textarea}
-                as="textarea"
-              />
-              <ErrorMessage
-                component="span"
-                name="content"
-                className={css.error}
-              />
-            </fieldset>
+        <fieldset className={css.formGroup}>
+          <label htmlFor="tag">Tag</label>
+          <Field as="select" id="tag" name="tag" className={css.select}>
+            <option value="Todo">Todo</option>
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+            <option value="Meeting">Meeting</option>
+            <option value="Shopping">Shopping</option>
+          </Field>
+          <ErrorMessage component="span" name="tag" className={css.error} />
+        </fieldset>
 
-            <fieldset className={css.formGroup}>
-              <label htmlFor="tag">Tag</label>
-              <Field as="select" id="tag" name="tag" className={css.select}>
-                <option value="Todo">Todo</option>
-                <option value="Work">Work</option>
-                <option value="Personal">Personal</option>
-                <option value="Meeting">Meeting</option>
-                <option value="Shopping">Shopping</option>
-              </Field>
-              <ErrorMessage component="span" name="tag" className={css.error} />
-            </fieldset>
-
-            <fieldset className={css.actions}>
-              <button
-                type="button"
-                className={css.cancelButton}
-                onClick={onClose}
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className={css.submitButton}
-                disabled={false}
-              >
-                Create note
-              </button>
-            </fieldset>
-          </Form>
-        );
-      }}
+        <fieldset className={css.actions}>
+          <button type="button" className={css.cancelButton} onClick={onClose}>
+            Cancel
+          </button>
+          <button type="submit" className={css.submitButton} disabled={false}>
+            Create note
+          </button>
+        </fieldset>
+      </Form>
     </Formik>
   );
 }
